d5897cc12a26d860024f84d8df0d2e1ad32299a3
samueloliveiraf <samueldiablo73@gmail.com>
2022-08-23 08:37:51 -0300

    add ajustes

--- src/pages/EmailReset.vue
+++ src/pages/EmailReset.vue
@@ -1,15 +1,11 @@
 <template>
   <q-page padding>
-    <q-card class="my-card window-height justify-center items-center">
-      <q-card-section>
-        <p class="text-body1">
-          Email enviado com sucesso!
-        </p>
-        <p class="text-body1">
-          Por favor, verifique seu Email: <strong>{{ $route.query.email }}</strong>
-        </p>
-      </q-card-section>
-    </q-card>
+    <p class="text-body1">
+      Email enviado com sucesso!
+    </p>
+    <p class="text-body1">
+      Por favor, verifique seu Email: <strong>{{ $route.query.email }}</strong>
+    </p>
   </q-page>
 </template>
 
